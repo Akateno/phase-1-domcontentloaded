fc352872904b7071d8360c1840bdee3f4d3a9d90
Erick Vargas <erickkvargas@gmail.com>
2022-04-11 14:13:38 -0700

    commit tree 4cbef02d5f0963f716895d21c66953693dc7b472

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    let sneeze = document.getElementById("text")
+    console.log(sneeze.textContent = "This is really cool!")
+})
